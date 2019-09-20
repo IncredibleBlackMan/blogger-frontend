bf373f2d34d1c5d7d5e44636b50f21290b06df58
Ramon3162 <ramonomondi@gmail.com>
2019-09-21 02:25:14 +0300

    Add templates

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blogger-Frontend
+
+This is the frontend for a Blog application that I use for learning and doing all the stuff I consider as being fun that can fall within this scope.
---
+++ docs/static/js/main.js
@@ -0,0 +1,10 @@
+const openNav = () => {
+    let nav = document.getElementById("top-navigation");
+    nav.classList.toggle("responsive");
+}
+
+const dropNav = () => {
+    let nav = document.getElementById("dropdwn");
+    nav.classList.toggle("dropdown-show");
+    
+}
