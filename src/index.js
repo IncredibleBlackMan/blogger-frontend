11e3b95c7466ff925ccb62c4e4140608467fe1b5
Ramon3162 <ramonomondi@gmail.com>
2019-10-09 01:25:56 +0300

    Setup redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import storeConfig from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory();
+const store = storeConfig();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root'),
+);
+
 serviceWorker.unregister();
